b0aea24302d79e88be84639f25cc39a9ec00d8e2
GGG KILLER <gggkiller2@gmail.com>
2015-07-16 08:38:12 -0300

    1.0

---
+++ CSSAndJsMinifier/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CSSAndJsMinifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(342, 52);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select A Folder";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CSS and Javascript Minifier";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 22);
+            this.label2.MaximumSize = new System.Drawing.Size(289, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "No File Selected!";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 110);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(342, 184);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Files Found";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(258, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Files with .min.js or .min.css are automatically ignored.";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(7, 20);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(329, 147);
+            this.listBox1.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.progressBar1);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(12, 300);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(342, 69);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Progress";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Status:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "label5";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(10, 36);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(326, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 383);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(336, 30);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Minify and Compress all CSS and Js";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 425);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CSS & Js Minifier";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CSSAndJsMinifier/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Web;
+
+namespace CSSAndJsMinifier
+{
+    public partial class Form1 : Form
+    {
+        string[] js, css;
+        string folder;
+        public Form1()
+        {
+            InitializeComponent();
+            label5.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderSelectDialog fsd = new FolderSelectDialog();
+            fsd.ShowDialog();
+            folder = fsd.FileName;
+            if(!Directory.Exists(folder))
+            {
+                MessageBox.Show("Directory doesn't exists!", "Error!");
+                folder = null;
+                return;
+            }
+            label2.Text = folder;
+            SearchCSSAndJS(folder,false);
+        }
+
+        private async void MinifyCSS(string filename)
+        {
+            SetStatus("Minifying " + filename.Replace(folder, "").Replace(".css", ".min.css") + " ...", 50);
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>{{ "input", File.ReadAllText(filename) }};
+
+                var content = new FormUrlEncodedContent(values);
+
+                var response = await client.PostAsync("http://cssminifier.com/raw", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                File.WriteAllText(filename.Replace(".css", ".min.css"), responseString);
+
+                SetStatus("Compressing " + filename.Replace(folder, "").Replace(".css", ".min.css") + " ...", 75);
+                Compress(filename.Replace(".css", ".min.css"));
+                SetStatus("Minifying and compressing of " + filename.Replace(folder, "").Replace(".css", ".min.css") + " done!", 100);
+            }
+        }
+
+        private async void MinifyJS(string filename)
+        {
+            using (var client = new HttpClient())
+            {
+                SetStatus("Minifying " + filename.Replace(folder, "").Replace(".js", ".min.js") + " ...", 50);
+                var values = new Dictionary<string, string> { { "input", File.ReadAllText(filename) } };
+
+                var content = new FormUrlEncodedContent(values);
+
+                var response = await client.PostAsync("http://javascript-minifier.com/raw", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                File.WriteAllText(filename.Replace(".js", ".min.js"), responseString);
+
+                SetStatus("Compressing " + filename.Replace(folder,"").Replace(".js", ".min.js") + " ...", 75);
+                Compress(filename.Replace(".js", ".min.js"));
+                SetStatus("Minifying and compressing of " + filename.Replace(folder, "").Replace(".js", ".min.js") + " done!", 100);
+            }
+        }
+
+        private void Compress(string filename)
+        {
+            FileInfo fin = new FileInfo(filename);
+            using (FileStream originalFileStream = fin.OpenRead())
+            {
+                if ((File.GetAttributes(fin.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden & fin.Extension != ".gz")
+                {
+                    using (FileStream compressedFileStream = File.Create(fin.FullName + ".gz"))
+                    {
+                        using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionLevel.Optimal))
+                        {
+                            originalFileStream.CopyTo(compressionStream);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SearchCSSAndJS(string path, bool minify)
+        {
+            js = Directory.GetFiles(path, "*.js", SearchOption.AllDirectories);
+            css = Directory.GetFiles(path, "*.css", SearchOption.AllDirectories);
+            foreach (string file in js)
+            {
+                if (file.IndexOf(".min.js") > -1) continue;
+
+                if (minify)
+                {
+                    SetStatus("Compressing " + Path.GetFileName(file) + " ...", 0);
+                    Compress(file);
+                    MinifyJS(file);
+                }
+                else
+                {
+                    listBox1.Items.Add(file.Replace(folder, "")); ;
+                }
+            }
+
+            foreach (string file in css)
+            {
+                if (file.IndexOf(".min.css") > -1) continue;
+
+                if (minify)
+                {
+                    SetStatus("Compressing " + Path.GetFileName(file) + " ...", 0);
+                    Compress(file);
+                    MinifyCSS(file);
+                }
+                else
+                {
+                    listBox1.Items.Add(file.Replace(folder,""));;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(!(folder is string))
+            {
+                MessageBox.Show("No folder selected or unexisting folder selected!");
+                return;
+            }
+            SearchCSSAndJS(folder, true);
+        }
+
+        private void SetStatus(string stats, int progress)
+        {
+            label5.Text = stats;
+            progressBar1.Value = progress;
+        }
+    }
+}
